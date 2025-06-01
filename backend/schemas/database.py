721bc66d7f5c15a2aa2257c112dab3f8fd0411a7
Nikita <152098327+Nihihitik@users.noreply.github.com>
2025-06-01 10:11:20 +0300

    Создание миграции базы данных

--- backend/schemas/database.py
+++ backend/schemas/database.py
@@ -2,30 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-import os
-from dotenv import load_dotenv
+# Хардкод URL для базы данных с явным портом
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/university"
 
-# Загружаем переменные окружения из .env файла
-load_dotenv()
-
-# Получаем данные подключения из переменных окружения
-DB_TYPE = os.getenv("DB_TYPE")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-
-# Формируем строку подключения в зависимости от типа базы данных
-if DB_TYPE == "postgresql":
-    SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-else:
-    raise ValueError("Неподдерживаемый тип базы данных")
-
-
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# Создаем подключение к базе данных
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Dependency для FastAPI
